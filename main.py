db47d905a58cfbb56cff7f095935f13b1410d388
agatawoz <agatex2003@gmail.com>
2023-11-26 21:10:07 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+f1 = open("wydatki.txt", "a+")
+f2 = open("limit.txt", "a+")
+
+
+def add():
+    k = input("kwota: ")
+    d = input("data: ")
+    o = input("opis: ")
+    all = f"kwota: {k} zl , data: {d}, opis: {o} \n"
+    f1.write(all)
+
+
+def set_limit():
+    print()
+
+
+def check_limit():
+    print()
